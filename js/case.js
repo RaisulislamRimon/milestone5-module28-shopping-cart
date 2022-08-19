a77f661144fd37ee2729ab81c1a73a86712b4aec
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-19 05:08:38 +0000

    update

--- js/case.js
+++ js/case.js
@@ -1,22 +1,43 @@
-function updateCaseNumber(elementId, updateOption) {
-  const caseNumberField = document.getElementById(elementId);
+// function updateCaseNumber(elementId, updateOption) {
+function updateCaseNumber(isIncrease) {
+  const caseNumberField = document.getElementById("case-number-field");
   const caseNumberString = caseNumberField.value;
   const previousCaseNumber = parseInt(caseNumberString);
-  if (updateOption === "+") {
-    const newCaseNumber = previousCaseNumber + 1;
-    caseNumberField.value = newCaseNumber;
-  } else if (updateOption === "-") {
-    const newCaseNumber = previousCaseNumber - 1;
-    caseNumberField.value = newCaseNumber;
+
+  let newCaseNumber;
+
+  //   if (updateOption === "+") {
+  //     const newCaseNumber = previousCaseNumber + 1;
+  //     caseNumberField.value = newCaseNumber;
+  //   } else if (updateOption === "-") {
+  //     const newCaseNumber = previousCaseNumber - 1;
+  //     caseNumberField.value = newCaseNumber;
+  //   }
+  if (isIncrease) {
+    newCaseNumber = previousCaseNumber + 1;
+  } else {
+    newCaseNumber = previousCaseNumber - 1;
   }
+  caseNumberField.value = newCaseNumber;
+  return newCaseNumber;
 }
 
 document.getElementById("btn-case-plus").addEventListener("click", function () {
-  updateCaseNumber("case-number-field", "+");
+  //   updateCaseNumber("case-number-field", "+");
+  const newCaseNumber = updateCaseNumber(true);
+  const caseTotalPrice = newCaseNumber * 59;
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
 });
 
 document
   .getElementById("btn-case-minus")
   .addEventListener("click", function () {
-    updateCaseNumber("case-number-field", "-");
+    // updateCaseNumber("case-number-field", "-");
+    updateCaseNumber(false);
+    const caseTotalElement = document.getElementById("case-total");
+    const caseTotalElementString = caseTotalElement.innerText;
+    const caseTotalPrice = parseInt(caseTotalElementString);
+    const newCaseTotalPrice = caseTotalPrice - 59;
+    caseTotalElement.innerText = newCaseTotalPrice;
   });
