314f6534c2c8ac8e5238ec54eaa4a5567b818fee
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-08-19 04:42:41 +0000

    update

--- js/case.js
+++ js/case.js
@@ -1,19 +1,22 @@
-function updateCaseNumber() {}
-
-document.getElementById("btn-case-plus").addEventListener("click", function () {
-  const caseNumberField = document.getElementById("case-number-field");
+function updateCaseNumber(elementId, updateOption) {
+  const caseNumberField = document.getElementById(elementId);
   const caseNumberString = caseNumberField.value;
   const previousCaseNumber = parseInt(caseNumberString);
-  const newCaseNumber = previousCaseNumber + 1;
-  caseNumberField.value = newCaseNumber;
+  if (updateOption === "+") {
+    const newCaseNumber = previousCaseNumber + 1;
+    caseNumberField.value = newCaseNumber;
+  } else if (updateOption === "-") {
+    const newCaseNumber = previousCaseNumber - 1;
+    caseNumberField.value = newCaseNumber;
+  }
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  updateCaseNumber("case-number-field", "+");
 });
 
 document
   .getElementById("btn-case-minus")
   .addEventListener("click", function () {
-    const caseNumberField = document.getElementById("case-number-field");
-    const caseNumberString = caseNumberField.value;
-    const previousCaseNumber = parseInt(caseNumberString);
-    const newCaseNumber = previousCaseNumber - 1;
-    caseNumberField.value = newCaseNumber;
+    updateCaseNumber("case-number-field", "-");
   });
